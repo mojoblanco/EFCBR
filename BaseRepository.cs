72c325a05740d352475053e718090260cdf2b8ce
MOJ <mojoblanco@gmail.com>
2018-07-25 02:29:44 +0100

    Updated namespaces

--- BaseRepository.cs
+++ BaseRepository.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 
-namespace EfCoreBaseRepo.Repository
+namespace EfCoreBaseRepo
 {
     public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : BaseEntity
     {
