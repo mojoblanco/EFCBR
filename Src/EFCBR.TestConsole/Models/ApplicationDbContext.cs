a19ce58ed12b89ea4df99fb66ff32b836d52df74
John Olawale <jolawale@softalliance.com>
2018-09-06 14:43:28 +0100

    Create test console app

---
+++ Src/EFCBR.TestConsole/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCBR.TestConsole.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\;Database=EFCoreDemo;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+    }
+}
---
+++ Src/EFCBR.TestConsole/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace EFCBR.TestConsole.Models
+{
+    public class Student : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}
